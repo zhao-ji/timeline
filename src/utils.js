0fef58b43d4d6b45aed96f7a948da885e3273cf3
Subs <BytesWarning@gmail.com>
2020-04-22 15:11:59 +1200

    Issue #master - filter out the hiragana and katakana japanese character

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 const chineseRegex = /[\u4e00-\u9fff]/;
+const japaneseRegex = /[\u3040-\u30ff]/;
 
 export default function checkIfMandarin(text) {
     if (typeof text !== "string") return false;
+    if (japaneseRegex.test(text)) return false;
     return chineseRegex.test(text);
 }
